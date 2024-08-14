a5b86907659321ebe46d195cc5b080b03387cd03
EliRodrEs <elisabetre.main@gmail.com>
2024-08-14 18:43:09 +0200

    fix: add min value validator to serializers.py

--- api/apps/payment/serializers.py
+++ api/apps/payment/serializers.py
@@ -1,7 +1,14 @@
+from django.core.validators import MinValueValidator
 from rest_framework import serializers
 from .models import Payment
+
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+        extra_kwargs = {
+            'source_amount': {
+                'validators': [MinValueValidator(0, "Amount must be positive")]
+            }
+        }
