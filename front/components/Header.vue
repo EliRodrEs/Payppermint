5d708b3e855104040f1c4bf461f5adc9e4f2da52
EliRodrEs <elisabetre.main@gmail.com>
2024-08-15 19:06:30 +0200

    feat: Add new Header component

--- front/components/Header.vue
+++ front/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sticky-top py-1 text-center d-flex align-items-center justify-content-center bg-[var(--zexel-error)]"
+    <div class="sticky top-0 py-1 text-center d-flex align-items-center justify-content-center bg-[var(--zexel-error)] text-[var(--zexel-pure-white)]"
         style="height: 50px;">
         <h1>HEADER HERE!</h1>
     </div>
