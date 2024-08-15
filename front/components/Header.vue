4e56a7d18cad2dfbf037a24e3c33796ed058b005
EliRodrEs <elisabetre.main@gmail.com>
2024-08-15 19:45:09 +0200

    fix: header responsiveness

--- front/components/Header.vue
+++ front/components/Header.vue
@@ -1,19 +1,19 @@
 <template>
     <navbar class="sticky top-0 text-center bg-[var(--zexel-success)] text-[var(--zexel-pure-white)] h-[4rem] p-4 flex justify-between items-center">
-        <router-link to="/" class="block">
-            <div class="max-w-[2rem] flex items-end gap-2 cursor-pointer transform transition-transform duration-500 ease-in-out hover:scale-110">
-                <img src="../public/img/mint_white.svg" alt="mint_logo">
-                <h1 class="font-bold text-[var(--zexel-pure-white)] text-xl">PAYPPERMINT</h1>
-            </div>
-        </router-link>
-        <div class="menu text-[var(--zexel-pure-white)] flex gap-4">
-            <NuxtLink to="/payments" class="transform transition-transform duration-500 ease-in-out hover:scale-110">
-                <div class="h5 m-0">{{ $t('payments') }}</div>
-            </NuxtLink>
-            <NuxtLink to="/countries" class="transform transition-transform duration-500 ease-in-out hover:scale-110">
-                <div class="h5 m-0">{{ $t('countries') }}</div>
-            </NuxtLink>
+      <router-link to="/" class="block">
+        <div class="max-w-[2rem] flex items-end gap-2 cursor-pointer transform transition-transform duration-500 ease-in-out hover:scale-110">
+          <img src="../public/img/mint_white.svg" alt="mint_logo">
+          <h1 class="font-bold text-[var(--zexel-pure-white)] text-xl hidden sm:block">PAYPPERMINT</h1>
         </div>
+      </router-link>
+      <div class="menu text-[var(--zexel-pure-white)] flex gap-4">
+        <NuxtLink to="/payments" class="transform transition-transform duration-500 ease-in-out hover:scale-110">
+          <div class="m-0 text-sm sm:text-base text-[var(--zexel-pure-white)]">{{ $t('payments') }}</div>
+        </NuxtLink>
+        <NuxtLink to="/countries" class="transform transition-transform duration-500 ease-in-out hover:scale-110">
+          <div class="m-0 text-sm sm:text-base text-[var(--zexel-pure-white)]">{{ $t('countries') }}</div>
+        </NuxtLink>
+      </div>
     </navbar>
   </template>
   
