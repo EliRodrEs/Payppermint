7ed04765711bc5e2ee4a748a13756464a36b9fac
EliRodrEs <elisabetre.main@gmail.com>
2024-08-17 20:17:00 +0200

    feat: Add currency and country selectors

--- front/pages/payments/PaymentForm.vue
+++ front/pages/payments/PaymentForm.vue
@@ -6,73 +6,87 @@
         <div class="col-md-4">
           <label for="sourceAmount" class="form-label">{{ $t('source_amount') }}</label>
           <input type="number" class="form-control" id="sourceAmount" v-model="payment.source_amount" required>
-          <div v-if="validationErrors.source_amount" class="text-red-500 text-sm">{{
-            validationErrors.source_amount }}</div>
+          <div v-if="validationErrors.source_amount" class="text-red-500 text-sm">{{ validationErrors.source_amount }}</div>
         </div>
         <div class="col-md-4">
-          <label for="sourceCurrency" class="form-label">{{ $t('source_currency') }}</label>
-          <input type="text" class="form-control" id="sourceCurrency" v-model="payment.source_currency" required>
-          <div v-if="validationErrors.source_currency" class="text-red-500 text-sm">{{
-            validationErrors.source_currency }}</div>
-        </div>
-        <div class="col-md-4">
-          <label for="sourceCountry" class="form-label">{{ $t('source_country') }}</label>
-          <input type="text" class="form-control" id="sourceCountry" v-model="payment.source_country" required>
-          <div v-if="validationErrors.source_country" class="text-red-500 text-sm">{{
-            validationErrors.source_country }}</div>
-        </div>
+        <label for="sourceCurrency" class="form-label">{{ $t('source_currency') }}</label>
+        <select id="sourceCurrency" class="form-control" v-model="payment.source_currency" required>
+          <option value="" disabled>Select currency</option>
+          <option v-for="currency in currencyOptions" :key="currency.value" :value="currency.value">
+            {{ currency.label }}
+          </option>
+        </select>
+        <div v-if="validationErrors.source_currency" class="text-red-500 text-sm">{{ validationErrors.source_currency }}</div>
+      </div>
+
+      <div class="col-md-4">
+        <label for="sourceCountry" class="form-label">{{ $t('source_country') }}</label>
+        <select id="sourceCountry" class="form-control" v-model="payment.source_country" required>
+          <option value="" disabled>Select country</option>
+          <option v-for="country in countryOptions" :key="country.value" :value="country.value">
+            {{ country.label }}
+          </option>
+        </select>
+        <div v-if="validationErrors.source_country" class="text-red-500 text-sm">{{ validationErrors.source_country }}</div>
+      </div>
       </div>
       <div class="row mb-3">
         <div class="col-md-4">
           <label for="targetAmount" class="form-label">{{ $t('target_amount') }}</label>
           <input type="number" class="form-control" id="targetAmount" v-model="payment.target_amount" required>
-          <div v-if="validationErrors.target_amount" class="text-red-500 text-sm">{{
-            validationErrors.target_amount }}</div>
+          <div v-if="validationErrors.target_amount" class="text-red-500 text-sm">{{ validationErrors.target_amount }}</div>
         </div>
         <div class="col-md-4">
-          <label for="targetCurrency" class="form-label">{{ $t('target_currency') }}</label>
-          <input type="text" class="form-control" id="targetCurrency" v-model="payment.target_currency" required>
-          <div v-if="validationErrors.target_currency" class="text-red-500 text-sm">{{
-            validationErrors.target_currency }}</div>
-        </div>
-        <div class="col-md-4">
-          <label for="targetCountry" class="form-label">{{ $t('target_country') }}</label>
-          <input type="text" class="form-control" id="targetCountry" v-model="payment.target_country" required>
-          <div v-if="validationErrors.target_country" class="text-red-500 text-sm">{{
-            validationErrors.target_country }}</div>
-          <div v-if="validationErrors.country_mismatch" class="text-red-500 text-sm mt-2">{{
-            validationErrors.country_mismatch }}</div>
+  <label for="targetCurrency" class="form-label">{{ $t('target_currency') }}</label>
+  <select id="targetCurrency" class="form-control" v-model="payment.target_currency" required>
+    <option value="" disabled>Select currency</option>
+    <option v-for="currency in currencyOptions" :key="currency.value" :value="currency.value">
+      {{ currency.label }}
+    </option>
+  </select>
+  <div v-if="validationErrors.target_currency" class="text-red-500 text-sm">{{ validationErrors.target_currency }}</div>
+</div>
 
-        </div>
+<div class="col-md-4">
+  <label for="targetCountry" class="form-label">{{ $t('target_country') }}</label>
+  <select id="targetCountry" class="form-control" v-model="payment.target_country" required>
+    <option value="" disabled>Select country</option>
+    <option v-for="country in countryOptions" :key="country.value" :value="country.value">
+      {{ country.label }}
+    </option>
+  </select>
+  <div v-if="validationErrors.target_country" class="text-red-500 text-sm">{{ validationErrors.target_country }}</div>
+  <div v-if="validationErrors.country_mismatch" class="text-red-500 text-sm mt-2">{{ validationErrors.country_mismatch }}</div>
+</div>
+
       </div>
       <div class="row mb-3">
         <div class="col-md-4">
           <label for="concept" class="form-label">{{ $t('concept') }}</label>
           <input type="text" class="form-control" id="concept" v-model="payment.concept" required>
-          <div v-if="validationErrors.concept" class="text-red-500 text-sm">{{ validationErrors.concept }}
-          </div>
+          <div v-if="validationErrors.concept" class="text-red-500 text-sm">{{ validationErrors.concept }}</div>
         </div>
         <div class="col-md-4">
           <label for="senderFullName" class="form-label">{{ $t('sender_full_name') }}</label>
           <input type="text" class="form-control" id="senderFullName" v-model="payment.sender_full_name" required>
-          <div v-if="validationErrors.sender_full_name" class="text-red-500 text-sm">{{
-            validationErrors.sender_full_name }}</div>
+          <div v-if="validationErrors.sender_full_name" class="text-red-500 text-sm">{{ validationErrors.sender_full_name }}</div>
         </div>
         <div class="col-md-4">
           <label for="receiverFullName" class="form-label">{{ $t('receiver_full_name') }}</label>
           <input type="text" class="form-control" id="receiverFullName" v-model="payment.receiver_full_name" required>
-          <div v-if="validationErrors.receiver_full_name" class="text-red-500 text-sm">{{
-            validationErrors.receiver_full_name }}</div>
+          <div v-if="validationErrors.receiver_full_name" class="text-red-500 text-sm">{{ validationErrors.receiver_full_name }}</div>
         </div>
       </div>
 
       <div id="buttonHolder">
-        <button v-if="!isSendingPayment" type="submit" class="btn btn-primary w-full h-12 px-4 py-2 text-lg cursor-pointer 
-            bg-[var(--zexel-azure)] 
-            border-[var(--zexel-azure)]
-            disabled:bg-[var(--zexel-disabled)] 
-            disabled:border-[var(--zexel-disabled)]" :disabled="!isFormValid">{{ buttonLabel }}</button>
-
+        <button v-if="!isSendingPayment" type="submit" 
+          class="btn btn-primary w-full h-12 px-4 py-2 text-lg cursor-pointer 
+          bg-[var(--zexel-azure)] 
+          border-[var(--zexel-azure)]
+          disabled:bg-[var(--zexel-disabled)] 
+          disabled:border-[var(--zexel-disabled)]"
+          :disabled="!isFormValid">{{ buttonLabel }}</button>
+        
         <div v-if="isSendingPayment" class="flex items-center justify-center w-full">
           <Spinner />
         </div>
@@ -84,7 +98,9 @@
 <script setup>
 import { ref, computed, defineProps, defineEmits } from 'vue'
 import Spinner from '~/components/ui/Spinner.vue';
-import { validateField, validators, isValidISOCurrencyCode, isvalidISOCountryCode, isSourceDifferentFromTarget } from './validators'
+import { validateField, validators, isValidISOCurrencyCode, isvalidISOCountryCode } from './validators'
+import { countryOptions } from './validISOCountryCodes'
+import { currencyOptions } from './validISOCurrencyCodes';
 
 
 const props = defineProps({
