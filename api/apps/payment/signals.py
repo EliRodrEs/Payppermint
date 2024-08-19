7faca82d8e70700ab9e9420ca89c7f75451b03b6
EliRodrEs <elisabetre.main@gmail.com>
2024-08-19 20:41:13 +0200

    chore: edit signals.py

--- api/apps/payment/signals.py
+++ api/apps/payment/signals.py
@@ -1,21 +1,20 @@
-# from django.db.models.signals import pre_save
-# from django.dispatch import receiver
-# from forex_python.converter import CurrencyRates, CurrencyCodes
-#
-# from .models import Payment
-# from .services import CurrencyConverterService
-#
-# from decimal import Decimal
-#
-#
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from forex_python.converter import CurrencyRates, CurrencyCodes
+
+from .models import Payment
+from .services import CurrencyConverterService
+
+from decimal import Decimal
+
+#TODO: Fix signal JSONDecodeError get_rate method has some issues
 # @receiver(pre_save, sender=Payment)
 # def update_target_amount(sender, instance, **kwargs):
 #     if instance.pk:
 #         if instance.rate_exchange == 0 or instance.rate_exchange is None:
 #             currency_rates = CurrencyRates()
 #             currency_codes = CurrencyCodes()
-#             currency_converter_service = CurrencyConverterService(currency_rates, currency_codes)
-#             instance.rate_exchange = currency_converter_service.currency_rates.get_rate(
+#             instance.rate_exchange = CurrencyConverterService(currency_rates, currency_codes).get_rate(
 #                 instance.source_currency, instance.target_currency
 #             )
 #
